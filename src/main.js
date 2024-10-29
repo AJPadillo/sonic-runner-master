c56806bd5f2b1876eed432149539a8b42f0f98f9
AJPadillo <ajpadillo@gmail.com>
2024-10-29 12:09:03 +0100

    agregado asset platforms en main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import k from "./kaplayCtx";
 
-k.loadSprite("chemical-bg", "graphics/chemical-bg.png")
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+

3b91bd75b51a4e55330f80b7d79577784466ab59
AJPadillo <ajpadillo@gmail.com>
2024-10-30 13:21:45 +0100

    agregado movimiento de platforms

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -17,12 +17,20 @@ export default function mainMenu() {
     ];
 
     k.onUpdate(() => {
-        if(bgPieces[1].pos.x < 0){
+        if (bgPieces[1].pos.x < 0) {
             bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
             bgPieces.push(bgPieces.shift())
         }
 
         bgPieces[0].move(-100, 0)
         bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
-    })
+
+        if (platforms[1].pos.x < 0) {
+            platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
+            platforms.push(platforms.shift());
+        }
+
+        platforms[0].move(-4000, 0);
+        platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450)
+    });
 }

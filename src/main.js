a21acc36f36d407d57714ed0b62c48d724413356
AJPadillo <ajpadillo@gmail.com>
2024-10-30 12:59:00 +0100

    creada funcion mainMenu parcial y prueba 1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import k from "./kaplayCtx";
+import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
 k.loadSprite("platforms", "graphics/platforms.png");
@@ -37,16 +38,10 @@ k.loadSound("jump", "sounds/Jump.wav");
 k.loadSound("ring", "sounds/Ring.wav");
 k.loadSound("city", "sounds/city.mp3");
 
-k.scene("main-menu", () => {
+k.scene("main-menu", mainMenu)
 
-})
+k.scene("game", () => {})
 
-k.scene("game", () => {
-
-})
-
-k.scene("gameover", () => {
-
-})
+k.scene("gameover", () => {})
 
 k.go("main-menu")

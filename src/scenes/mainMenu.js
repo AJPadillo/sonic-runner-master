f3cb8ff24c553a8125ae2e6ea4db81f967b27ed4
AJPadillo <ajpadillo@gmail.com>
2024-10-30 13:12:20 +0100

    agregado desplazamiento automático de chemical-bg

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -15,4 +15,14 @@ export default function mainMenu() {
         k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
         k.add([k.sprite("platforms"), k.pos(platformWidth * 4, 450), k.scale(4)]),
     ];
+
+    k.onUpdate(() => {
+        if(bgPieces[1].pos.x < 0){
+            bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+            bgPieces.push(bgPieces.shift())
+        }
+
+        bgPieces[0].move(-100, 0)
+        bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+    })
 }

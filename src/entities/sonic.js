a332d1240cb246c236c7c6f656f34847afd314be
AJPadillo <ajpadillo@gmail.com>
2024-10-31 11:49:28 +0100

    agregada scale, area, anchor y pos a makeSonic

--- src/entities/sonic.js
+++ src/entities/sonic.js
@@ -1,5 +1,10 @@
 export function makeSonic(pos) {
     const sonic = k.add([
-        k.sprite("sonic", {anim: "run"})
+        k.sprite("sonic", {anim: "run"}),
+        k.scale(4),
+        k.area(),
+        k.anchor("center"),
+        k.pos(pos),
+        
     ])
 }

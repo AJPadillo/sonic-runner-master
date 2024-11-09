09b4cf12af92005fe069f98b48add7a8697cbf6a
AJPadillo <ajpadillo@gmail.com>
2024-11-09 09:33:38 +0100

    agregada logica de asignacion de rangos y valores al score en gameOver.js

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -7,4 +7,17 @@ export default function gameOver(citySfx) {
 
     const rankGrades = ["F", "E", "D", "C", "B", "A", "S"];
     const rankValues = [50, 280, 500, 720, 980, 1200, 1780];
+
+    let currentRank = "F";
+    let bestRank = "F";
+
+    for (let i = 0; i < rankValues.length; i++) {
+        if (rankValues[i] < currentScore) {
+            currentRank = rankGrades[i];
+        }
+        
+        if (rankValues[i] < bestScore) {
+            bestRank = rankGrades[i];
+        }
+    }
 }

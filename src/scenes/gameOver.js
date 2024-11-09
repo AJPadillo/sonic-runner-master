e0bf93701c8dbf51c4d2e6be2f18d05db690fddf
AJPadillo <ajpadillo@gmail.com>
2024-11-09 09:53:02 +0100

    creada logica de loop para nueva partida en gameOver.js

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -76,4 +76,16 @@ export default function gameOver(citySfx) {
         k.text(bestRank, { font: "mania", size: 100 }),
         k.anchor("center")
     ]);
+
+    k.wait(1, () => {
+        k.add([
+            k.text("Press Space/Click/Touch to Play Again", {
+                font: "mania",
+                size: 64
+            }),
+            k.anchor("center"),
+            k.pos(k.center().x, k.center().y + 350)
+        ]);
+        k.onButtonPress("jump", () => k.go("game"));
+    });
 }
